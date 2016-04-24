b5b49ddd29fd0191b76cec16170c62410628c8bd
Joey Figaro <joey@joeyfigaro.com>
2016-04-23 22:02:50 -0400

    added utils/tsv_converter

--- utils/tsv_converter.py
+++ utils/tsv_converter.py
@@ -1,3 +1,13 @@
 # Carly Gibson
+# Joey Figaro
 # Apr. 23, 2016
 # converts TSV files to CSV
+
+#
+#  Required modules
+#
+import os
+import inspect
+
+source = open(os.path.abspath('../2014-drug-use/_data/DS0001/data.tsv')).read()
+print(source)
