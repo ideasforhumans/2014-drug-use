b0b79c3c0cd0c6deb3043315c4a2c58d606628e9
Joey Figaro <joey@joeyfigaro.com>
2016-04-23 23:18:46 -0400

    moving to carly's machine`

--- utils/tsv_converter.py
+++ utils/tsv_converter.py
@@ -8,6 +8,16 @@
 #
 import os
 import inspect
+import logging
+import csv
+
+logging.basicConfig(level = logging.DEBUG, format='[%(levelname)s] %(asctime)s -- %(message)s')
+logging.debug('Converting .csv to .tsv...')
 
 source = open(os.path.abspath('../2014-drug-use/_data/DS0001/data.tsv')).read()
-print(source)
+
+print(csv.reader(source))
+
+reader_source = csv.reader(source)
+source_data = list(reader_source)
+print(source_data)
