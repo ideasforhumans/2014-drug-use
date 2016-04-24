1225195a3d3afc8532dace5dd4559e408fabf345
Carly Gibson <carlygibson@Carlys-MBP.fios-router.home>
2016-04-24 00:00:36 -0400

    updated conversion script

--- utils/tsv_converter.py
+++ utils/tsv_converter.py
@@ -14,10 +14,14 @@ import csv
 logging.basicConfig(level = logging.DEBUG, format='[%(levelname)s] %(asctime)s -- %(message)s')
 logging.debug('Converting .csv to .tsv...')
 
-source = open(os.path.abspath('../2014-drug-use/_data/DS0001/data.tsv')).read()
+source = open(os.path.abspath('../2014-drug-use/_data/DS0001/dummy.tsv')).read()
+output = open(os.path.abspath('../2014-drug-use/_data/DS0001/dummy.csv', 'w', newline = '')
 
-print(csv.reader(source))
+reader_source = csv.reader(source, dialect = csv.excel_tab)
+#source_data = list(reader_source)
+#writer_source = csv.writer(output)
 
-reader_source = csv.reader(source)
-source_data = list(reader_source)
-print(source_data)
+"""for row in source:
+    writer_source.writerow(row)
+    
+output.close()"""
